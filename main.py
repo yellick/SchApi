6db86fe52910bd96bffff9b86095ca00c92ee537
Mikhail <gubm04@mail.ru>
2025-03-17 02:27:14 +0500

    work please

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/check_connection', methods=['GET'])
 def check_connection():
     return 'true'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
